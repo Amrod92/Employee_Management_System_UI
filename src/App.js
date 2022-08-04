95fc5cbb965e750643e66803d9a599816f8b0b87
Manlio Chite <manlio.chite-cic@ibm.com>
2022-08-04 15:59:21 +0100

    Creating EmployeeList component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AddEmployee from "./components/AddEmployee";
+import EmployeeList from "./components/EmployeeList";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <AddEmployee />
-    </>
+      <Routes>
+        <Route index element={<EmployeeList />}></Route>
+        <Route path="/" element={<EmployeeList />}></Route>
+        <Route path="/list-employee" element={<EmployeeList />}></Route>
+        <Route path="/add-employee" element={<AddEmployee />}></Route>
+      </Routes>
+      
+    </BrowserRouter>
   );
 }
 
