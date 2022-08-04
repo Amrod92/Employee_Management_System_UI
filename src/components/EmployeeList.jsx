8fbf431350cbe84584071e431c69cb6caa360ae6
Manlio Chite <manlio.chite-cic@ibm.com>
2022-08-04 16:25:49 +0100

    Employee component refactoring

--- src/components/EmployeeList.jsx
+++ src/components/EmployeeList.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { useNavigate } from 'react-router-dom'
+import EmployeeService from '../services/EmployeeService';
+import Employee from './Employee';
 
 const EmployeeList = () => {
 
+const [loading, setLoading] = useState(true);
+const [employees, setEmployees] = useState(null);
+
 const navigate = useNavigate();
+
+useEffect(() => {
+    const fetchData = async () => {
+        setLoading(true);
+        try{
+            const response = await EmployeeService.getEmployees();
+            setEmployees(response.data);
+        }catch(error){
+            console.error(error)
+        }
+
+        setLoading(false)
+    }
+
+    fetchData();
+}, []);
 
   return (
     <div className='container mx-auto my-8'>
@@ -22,29 +43,13 @@ const navigate = useNavigate();
                         <th className="text-left font-medium text-gray-500 uppercase tracking-wider py-3 px-6">Actions</th>
                     </tr>
                 </thead>
+                {!loading && (
                 <tbody className='bt-white'>
-                    <tr>
-                        <td className='text-left px-6 py-4 whitespace-nowrap'>
-                            <div className='text-sm text-gray-500'>
-                                Manlio
-                            </div>
-                        </td>
-                        <td className='text-left px-6 py-4 whitespace-nowrap'>
-                            <div className='text-sm text-gray-500'>
-                                Chite
-                            </div>
-                        </td>
-                        <td className='text-left px-6 py-4 whitespace-nowrap'>
-                            <div className='text-sm text-gray-500'>
-                                manlio92--@live.it
-                            </div>
-                        </td>
-                        <td className='text-left px-6 py-4 whitespace-nowrap font-medium text-sm'>
-                            <a href='#' className='text-indigo-600 hover:text-indigo-800 px-4'>Edit</a>
-                            <a href='#' className='text-indigo-600 hover:text-indigo-800 px-4'>Delete</a>
-                        </td>
-                    </tr>
+                    { employees.map((emp => (
+                        <Employee employee={emp} key={emp.id}/>
+                    ))) }
                 </tbody>
+                )}
             </table>
         </div>
     </div>
