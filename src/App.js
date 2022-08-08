f14e2b81db8238d17a1f06383e7f1bc4e237bf9a
Manlio Chite <manlio.chite-cic@ibm.com>
2022-08-08 11:57:10 +0100

    Implementign update functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AddEmployee from "./components/AddEmployee";
 import EmployeeList from "./components/EmployeeList";
 import Navbar from "./components/Navbar";
+import UpdateEmployee from "./components/UpdateEmployee";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<EmployeeList />}></Route>
         <Route path="/list-employee" element={<EmployeeList />}></Route>
         <Route path="/add-employee" element={<AddEmployee />}></Route>
+        <Route path="/edit-employee/:id" element={<UpdateEmployee />}></Route>
       </Routes>
       
     </BrowserRouter>
--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -1,32 +1,45 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
-const Employee = ({employee, deleteEmployee}) => {
-  return (
-    <tr>
-        <td className='text-left px-6 py-4 whitespace-nowrap'>
-            <div className='text-sm text-gray-500'>
-                {employee.firstName}
-            </div>
-        </td>
-        <td className='text-left px-6 py-4 whitespace-nowrap'>
-            <div className='text-sm text-gray-500'>
-            {employee.lastName}
-            </div>
-        </td>
-        <td className='text-left px-6 py-4 whitespace-nowrap'>
-            <div className='text-sm text-gray-500'>
-            {employee.emailId}
-            </div>
-        </td>
-        <td className='text-left px-6 py-4 whitespace-nowrap font-medium text-sm'>
-            <a href='#' className='text-indigo-600 hover:text-indigo-800 px-4'>Edit</a>
-            <a 
-            href='#'
-            onClick={(e, id) => {deleteEmployee(e, employee.id)}}
-            className='text-indigo-600 hover:text-indigo-800 px-4 hover:cursor-pointer'>Delete</a>
-        </td>
-    </tr>
-  )
+const Employee = ({employee, deleteEmployee }) => {
+    const navigate = useNavigate();
+
+    const editEmployee = (e, id) => {
+        e.preventDefault();
+        navigate(`/edit-employee/${id}`);
+    };
+
+    return (
+        <tr>
+            <td className='text-left px-6 py-4 whitespace-nowrap'>
+                <div className='text-sm text-gray-500'>
+                    {employee.firstName}
+                </div>
+            </td>
+            <td className='text-left px-6 py-4 whitespace-nowrap'>
+                <div className='text-sm text-gray-500'>
+                {employee.lastName}
+                </div>
+            </td>
+            <td className='text-left px-6 py-4 whitespace-nowrap'>
+                <div className='text-sm text-gray-500'>
+                {employee.emailId}
+                </div>
+            </td>
+            <td className='text-left px-6 py-4 whitespace-nowrap font-medium text-sm'>
+                <a 
+                    onClick={(e, id) => {editEmployee(e, employee.id)}}
+                    href='#' 
+                    className='text-indigo-600 hover:text-indigo-800 px-4 hover:cursor-pointer'>
+                        Edit
+                </a>
+                <a 
+                href='#'
+                onClick={(e, id) => {deleteEmployee(e, employee.id)}}
+                className='text-indigo-600 hover:text-indigo-800 px-4 hover:cursor-pointer'>Delete</a>
+            </td>
+        </tr>
+    )
 }
 
 export default Employee
